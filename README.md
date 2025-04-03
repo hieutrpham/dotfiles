b9114bdf4acb6f363b496f8baeef50c6d9ac4cd6
Hieu Pham <hieutrungpham91@gmail.com>
2025-04-03 16:50:55 +0300

    update

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 ```
-sudo pacman -S stow tmux neovim fzf picom gcc make tree man-pages man-db git fish bat ghostty nitrogen thunar unzip xorg-xrandr
+sudo pacman -S stow tmux neovim fzf picom gcc make tree man-pages man-db git fish bat ghostty nitrogen thunar unzip xorg-xrandr xclip
 yay -S  microsoft-edge-stable-bin
 ```
+Additional Software I use:
+* yay (package manager)
+* kitty (terminal emulator)
--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,15 @@
 if status is-interactive
+    # Commands to run in interactive sessions can go here
     set -U fish_autosuggestion_enabled 1
     set -U fish_term24bit 1
+end
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
 end
 function fcd
     set dir (find . -type d | fzf)
@@ -8,10 +17,12 @@ function fcd
         cd "$dir"
     end
 end
-source ~/.bashrc
-#alias v='nvim'
-#alias gs="git status"
-#alias ga="git add ."
-#alias gc="git commit -m"
-#alias gp="git push"
-#alias gl="git pull --rebase"
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
+alias v='nvim'
+alias gs="git status"
+alias ga="git add ."
+alias gc="git commit -m"
+alias gp="git push"
+alias gl="git pull --rebase"
+alias nf='fzf -m --preview="bat --color=always {}" --bind "enter:become(nvim {+})"'
