5ab22921a43699f8f9ec65a57516cae2eb785648
Hieu Pham <hieutrungpham91@gmail.com>
2025-04-09 19:18:18 +0300

    update

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -39,7 +39,7 @@ alias ga="git add ."
 alias gc="git commit -m"
 alias gp="git push"
 alias gl="git pull --rebase"
-alias nf='fzf -m --preview="bat --color=always {}" --bind "enter:become(nvim {+})"'
+alias nf='fzf -m --preview="bat --style=numbers --color=always {}" --bind "enter:become(nvim {+})"'
 
 zoxide init --cmd cd fish | source
 set -gx EDITOR nvim
