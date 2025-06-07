99c567de32ae91fc80210825ddc301019f7dcb14
Hieu Pham <hieutrungpham91@gmail.com>
2025-06-07 11:16:42 +0300

    fix bug in i3config for scratchpad

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,3 +1,8 @@
+P = function(v)
+	print(vim.inspect(v))
+	return v
+end
+
 require("remap")
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
